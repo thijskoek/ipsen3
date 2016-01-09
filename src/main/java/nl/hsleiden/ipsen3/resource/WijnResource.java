977c1c6e3aa01cf3f7ad63102b7dc2fe3e37cecc
Daan Rosbergen <d.rosbergen@gmail.com>
2016-01-09 20:19:53 +0100

    Add WijnResource

---
+++ src/main/java/nl/hsleiden/ipsen3/resource/WijnResource.java
@@ -0,0 +1,53 @@
+package nl.hsleiden.ipsen3.resource;
+
+import com.codahale.metrics.annotation.Timed;
+import io.dropwizard.hibernate.UnitOfWork;
+import nl.hsleiden.ipsen3.core.Wijn;
+import nl.hsleiden.ipsen3.dao.WijnDAO;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by Daan on 30-Nov-15.
+ */
+@Path("wijn")
+@Produces(MediaType.APPLICATION_JSON)
+public class WijnResource {
+
+    private final String template;
+    private final String defaultName;
+    private final AtomicLong counter;
+    private final WijnDAO dao;
+
+    public WijnResource(String template, String defaultName, WijnDAO dao) {
+        this.template = template;
+        this.defaultName = defaultName;
+        this.counter = new AtomicLong();
+        this.dao = dao;
+    }
+
+    @GET
+    @Timed
+    @UnitOfWork
+    public List findAll() { return dao.findAll(); }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Timed
+    @UnitOfWork
+    public void create(Wijn wijn) { dao.create(wijn); }
+
+
+    @GET
+    @Path("{id}")
+    @Timed
+    @UnitOfWork
+    public Wijn getById(@PathParam("id") Long id) {
+        return dao.findById(id);
+    }
+
+
+}
