7b8f81bb68628b7c6a14897ff7304185015c6737
Daan Rosbergen <d.rosbergen@gmail.com>
2016-01-09 20:42:10 +0100

    Add Angular app

---
+++ public_html/app/scripts/services/wijnen.js
@@ -0,0 +1,43 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name appApp.wijnen
+ * @description
+ * # wijnen
+ * Service in the appApp.
+ */
+angular.module('appApp')
+  .service('wijnen', function ($http, $q) {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+    var url = 'http://localhost:8080/wijn';
+
+    return {
+
+      all: function() {
+        var deferred = $q.defer();
+        $http({
+          method: 'GET',
+          url: url
+        }).then(function(data){
+          deferred.resolve(data.data);
+        }, function(error){
+          deferred.reject(error);
+        });
+
+        return deferred.promise;
+      },
+
+      findById: function(id) {
+        return $http({
+          method: 'GET',
+          url: url + '/' + id
+        }).then(function(response){
+          return response.data
+        }, function(error){
+          return error;
+        });
+      }
+
+    };
+  });
