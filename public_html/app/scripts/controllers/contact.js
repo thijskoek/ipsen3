52fb95a302cf126e4180a4597cf2786f580c82e3
Brandon van Wijk <brandonvanwijk@gmail.com>
2016-01-10 14:07:35 +0100

    route naar contact toegevoegd aan app.js en contactcontroller aangemaakt

---
+++ public_html/app/scripts/controllers/contact.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name appApp.controller:ContactCtrl
+ * @description
+ * # ContactCtrl
+ * Controller of the appApp
+ */
+angular.module('appApp')
+  .controller('ContactCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
