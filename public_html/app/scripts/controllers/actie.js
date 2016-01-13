56d8b8c5f28bc531254f820a15fdb5ad4fc45b06
icyberneticorganism <thijskoekprivate@+subscriptions@gmail.com>
2016-01-13 14:55:25 +0100

    actie controller/bestellijst controller/+views en backend bestellijstmodel

--- public_html/app/scripts/controllers/actie.js
+++ public_html/app/scripts/controllers/actie.js
@@ -1,28 +1,32 @@
+/**
+ * Created by Thijs Koek on 1/13/2016.
+ */
+
 'use strict';
 
 /**
- * @ngdoc function
- * @name appApp.controller:ActieCtrl
- * @description
- * # ActieCtrl
- * Controller of the appApp
+ *@ngdoc function
+ *@name appAp.controller.ActieCtrl
+ *@description
+ *#ActieCtrl
+ *Controller of the appApp
  */
+
 angular.module('appApp')
-  .controller('ActieCtrl', function ($scope, wijnen) {
+  .controller('ActieCtrl',  function($scope, wijnen){
+    $scope.checkboxModel = {
+      value1 : true,
+    }
     this.awesomeThings = [
-      'HTML5 Boilerplate',
+      'HTML5 Bolerplate',
       'AngularJS',
       'Karma'
     ];
-
-    $scope.wijnen = [];
+    $scope.wijnen =[];
 
     wijnen.all().then(function(data) {
       $scope.wijnen = data;
     }, function() {
       throw Error;
     });
-
-
-
   });
