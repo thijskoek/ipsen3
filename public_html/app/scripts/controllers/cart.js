dbb9b89f0f46b5098fb77543105ddd6ad0cd0962
Brandon van Wijk <brandonvanwijk@gmail.com>
2016-01-14 10:14:32 +0100

    added totalprice in cart

--- public_html/app/scripts/controllers/cart.js
+++ public_html/app/scripts/controllers/cart.js
@@ -11,7 +11,24 @@ angular.module('appApp')
   .controller('CartCtrl', function ($scope, wijnen) {
 
     $scope.cartForm = {};
-    $scope.aantal = 0;
+    $scope.aantal = 1;
+
+    $scope.getTotal = function(wijn) {
+
+        return parseInt(wijn.aantal) * wijn.prijs;
+    }
+
+    $scope.getTotalCart = function() {
+      var total = 0;
+      //total.isNumber(0);
+
+      $scope.wijnen.forEach(function(wijn, index) {
+        total += parseInt(wijn.aantal) * wijn.prijs;
+        console.log(parseInt(wijn.aantal) * wijn.prijs);
+      })
+      return Math.round(total * 100) / 100
+
+    }
 
     $scope.checkAantal = function(aantal) {
         if(aantal == null) {
