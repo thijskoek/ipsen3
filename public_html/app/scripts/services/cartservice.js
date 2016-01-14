691fe875f3c43351e4178b1753bdd0c843f70b6a
Brandon van Wijk <brandonvanwijk@gmail.com>
2016-01-14 13:14:57 +0100

    added cartService

---
+++ public_html/app/scripts/services/cartservice.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name appApp.cartService
+ * @description
+ * # cartService
+ * Service in the appApp.
+ */
+angular.module('appApp')
+  .service('cartService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
