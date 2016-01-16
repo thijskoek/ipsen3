b2efb7d6cf36c73eb238b385cfd8f10bf85b2c9b
Brandon van Wijk <brandonvanwijk@gmail.com>
2016-01-16 21:58:40 +0100

    added orderService

---
+++ public_html/app/scripts/services/orderservice.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name appApp.orderService
+ * @description
+ * # orderService
+ * Service in the appApp.
+ */
+angular.module('appApp')
+  .service('orderService', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
